05975b54d623a23e617636109d3158acd66634e0
KhalidIbra <khalidrk@hotmail.co.uk>
2025-09-01 22:26:38 +0100

    removed dynamodb module and added ecr module

---
+++ infra/modules/ecr/main.tf
@@ -0,0 +1,49 @@
+resource "aws_ecr_repository" "urlapp" {
+  name                 = var.name
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  encryption_configuration {
+    encryption_type = "AES256"
+  }
+
+  
+}
+
+
+resource "aws_ecr_lifecycle_policy" "parameters" {
+  repository = aws_ecr_repository.this.name
+
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+        description  = "Keep last 10 images (any tags)"
+        selection = {
+          tagStatus     = "any"
+          countType     = "imageCountMoreThan"
+          countNumber   = 10
+        }
+        action = {
+          type = "expire"
+        }
+      },
+      {
+        rulePriority = 2
+        description  = "Expire untagged images older than 14 days"
+        selection = {
+          tagStatus   = "untagged"
+          countType   = "sinceImagePushed"
+          countUnit   = "days"
+          countNumber = 14
+        }
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+}
--- infra/modules/ecs/main.tf
+++ infra/modules/ecs/main.tf
@@ -64,3 +64,14 @@ resource "aws_ecs_service" "ki_service" {
     target_group_arn = var.alb_target_group_arn
   }
 }
+
+resource "aws_dynamodb_table" "url_storage" {
+  name         = var.dynamodb_tablename
+  billing_mode = "PAY_PER_REQUEST"
+
+  hash_key = "UserId"
+  attribute {
+    name = "UserId"
+    type = "S"
+  }
+}
