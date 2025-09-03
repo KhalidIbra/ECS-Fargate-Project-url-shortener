bc56d216236631dade6dc0f1cfb623428f79f567
KhalidIbra <khalidrk@hotmail.co.uk>
2025-09-03 17:50:52 +0100

    modified waf module

---
+++ infra/modules/waf/main.tf
@@ -0,0 +1,67 @@
+resource "aws_wafv2_web_acl" "this" {
+  name        = "${var.name}-waf"
+  description = "WAF for ${var.name} ALB"
+  scope       = "REGIONAL" 
+
+  default_action {
+    allow {}
+  }
+
+  visibility_config {
+    cloudwatch_metrics_enabled = true
+    metric_name                = "${var.name}-waf-metrics"
+    sampled_requests_enabled   = true
+  }
+
+  
+  rule {
+    name     = "AWS-AWSManagedRulesCommonRuleSet"
+    priority = 1
+
+    override_action {
+      none {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesCommonRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "CommonRuleSet"
+      sampled_requests_enabled   = true
+    }
+  }
+
+  
+  rule {
+    name     = "AWS-AWSManagedRulesKnownBadInputsRuleSet"
+    priority = 2
+
+    override_action {
+      none {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesKnownBadInputsRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "KnownBadInputs"
+      sampled_requests_enabled   = true
+    }
+  }
+}
+
+
+resource "aws_wafv2_web_acl_association" "this" {
+  resource_arn = var.alb_arn
+  web_acl_arn  = aws_wafv2_web_acl.this.arn
+}
