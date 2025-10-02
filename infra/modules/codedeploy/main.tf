317672eb1dcf9c12a523012ceb7893c117b3fd34
KhalidIbra <khalidrk@hotmail.co.uk>
2025-10-02 20:55:02 +0100

    modified codedeploy main.tf

--- infra/modules/codedeploy/main.tf
+++ infra/modules/codedeploy/main.tf
@@ -8,6 +8,8 @@ resource "aws_codedeploy_deployment_group" "ki_ecs" {
   deployment_group_name = "ki-ecs-group"
   service_role_arn      = var.codedeploy_role_arn
 
+  deployment_config_name = "CodeDeployDefault.ECSCanary10Percent5Minutes"
+
   deployment_style {
     deployment_option = "WITH_TRAFFIC_CONTROL"
     deployment_type   = "BLUE_GREEN"
