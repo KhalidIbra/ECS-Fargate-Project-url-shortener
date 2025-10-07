3b627135d1a00738bc4193f8cbece39c97ac63b8
KhalidIbra <khalidrk@hotmail.co.uk>
2025-10-07 14:28:10 +0100

    modified target groups in alb module

--- infra/modules/alb/main.tf
+++ infra/modules/alb/main.tf
@@ -48,7 +48,7 @@ resource "aws_lb_listener" "http" {
 
   default_action {
     type             = "forward"
-     target_group_arn = aws_lb_target_group.blue.arn   
+     target_group_arn = aws_lb_target_group.blue.name  
   }
 
   lifecycle {
@@ -65,7 +65,7 @@ resource "aws_lb_listener" "https" {
 
   default_action {
     type = "forward"
-    target_group_arn = aws_lb_target_group.blue.arn
+    target_group_arn = aws_lb_target_group.blue.name
   }
 }
 
