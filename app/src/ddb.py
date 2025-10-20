5a9b6c185feb92d1629378ec18560d98eb81f0b6
KhalidIbra <khalidrk@hotmail.co.uk>
2025-10-20 22:25:32 +0100

    Edited ddb.py

--- app/src/ddb.py
+++ app/src/ddb.py
@@ -4,8 +4,8 @@ import os, boto3
 _table = boto3.resource("dynamodb").Table(os.environ["TABLE_NAME"])
 
 def put_mapping(short_id: str, url: str):
-    _table.put_item(Item={"id": short_id, "url": url})
+    _table.put_item(Item={"UserId": short_id, "url": url})
 
 def get_mapping(short_id: str):
-    resp = _table.get_item(Key={"id": short_id})
+    resp = _table.get_item(Key={"UserId": short_id})
     return resp.get("Item")
